d5b76e4002b52ef2797250a813b1949774b2a889
Tingan Ho <tingan87@gmail.com>
2014-01-16 15:45:10 +0800

    Fixed commenting for main function

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
 /**
+ * Because using nested jshint globals is not possible with
+ * the current grunt-contrib-jshint. We need to format the
+ * nested globals to reach top-level object properties. This
+ * formats your nested jshint globals.
  *
+ * @param {Object} opts
+ * @return {Object}
+ * @api public
  */
 
 module.exports = function(opts) {
